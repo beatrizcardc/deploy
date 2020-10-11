79f27a5089af951525319a2e30095572d0755403
beatrizcardc <72693632+beatrizcardc@users.noreply.github.com>
2020-10-11 11:42:23 -0300

    create index.js

---
+++ index.js
@@ -0,0 +1,58 @@
+let canvas = document.querySelector('canvas');
+let ctx_ = document.getElementById('chart').getContext('2d');
+
+let ctx = canvas.getContext('2d');
+let isWriting = false;
+
+let updateCanvas = () => {
+	canvas.height = window.innerHeight;
+	canvas.width = window.innerWidth;
+}
+
+let reset = () => {
+    updateCanvas();
+	pixels = [];
+	createGrid();
+}
+
+// Window Handlers.
+window.onload = updateCanvas();
+window.addEventListener('resize', e => reset());
+window.addEventListener('keyup', e => {
+    if(e.keyCode == 67) {
+        reset();
+    }
+});
+
+
+const gridHeight = 28;
+const gridWidth = 28;
+
+let mouseX = 0;
+let mouseY = 0;
+
+let pixels = [];
+
+class Pixel {
+	constructor(x, y, dim) {
+		this.x = x;
+		this.y = y;
+		this.isOn = false;
+		this.dim = dim;
+		this.bounding = [x, x + dim, y, y + dim];
+	}
+
+	draw() {
+		if (!this.isOn) {
+			ctx.fillStyle = '#0d0d0d';
+			ctx.strokeStyle = '#0d0d0d';
+		} else {
+			ctx.fillStyle = '#d3d3d3';
+			ctx.strokeStyle = '#d3d3d3'
+		}
+		ctx.beginPath();
+		ctx.fillRect(this.x, this.y, this.dim, this.dim);
+		ctx.lineWidth = 1;
+		ctx.strokeRect(this.x, this.y, this.dim, this.dim);
+	}
+}
